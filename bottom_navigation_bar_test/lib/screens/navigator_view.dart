63a14bfc2a19cf71f89969ae344a2704aae8ceb3
Alex <alexesc154@gmail.com>
2022-11-15 00:58:57 +0300

    add screens for navigations

--- bottom_navigation_bar_test/lib/screens/navigator_view.dart
+++ bottom_navigation_bar_test/lib/screens/navigator_view.dart
@@ -1,3 +1,6 @@
+import 'package:bottom_navigation_bar_test/screens/home_page.dart';
+import 'package:bottom_navigation_bar_test/screens/posts.dart';
+import 'package:bottom_navigation_bar_test/screens/setting.dart';
 import 'package:flutter/material.dart';
 
 class NavigatorView extends StatefulWidget {
@@ -8,13 +11,24 @@ class NavigatorView extends StatefulWidget {
 }
 
 class _NavigatorViewState extends State<NavigatorView> {
+  int indexScreen = 0;
+
+  List<Widget> myWidget = const [
+    HomePage(),
+    MyPosts(),
+    Settings(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Demo'),
+        title: const Text('Demo'),
       ),
-      body: Container(),
+      body: IndexedStack(
+        index: indexScreen,
+        children: myWidget,
+      ),
     );
   }
 }
